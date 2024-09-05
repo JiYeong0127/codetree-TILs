1a74493a334c30ee17a3937b44f2a3bae3fed902
JiYeong0127 <101117143+JiYeong0127@users.noreply.github.com>
2024-09-05 18:00:11 +0900

    Title: 장학금, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240905/장학금/scholarship.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int min, fin;
+    cin>> min>>fin;
+
+    if(min>=90){
+        if(fin>=90){
+            cout<<100000;
+        }else if(fin>=90){
+            cout<<50000;
+        }
+    }else{
+        cout<<0;
+    }
+    return 0;
+}
