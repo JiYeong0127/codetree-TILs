4c98b805761d6df5f8ca44d66a19784ab517f499
JiYeong0127 <101117143+JiYeong0127@users.noreply.github.com>
2024-09-05 17:47:47 +0900

    Title: 둘 중 하나의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240905/둘 중 하나의 배수/multiple-of-either.cpp
+++ 240905/둘 중 하나의 배수/multiple-of-either.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
+    cin>>a>>b; 
     if((a%3 == 0)||(a%5==0)){
         cout<<1;
     }else
     cout<<0;
+
     return 0;
 }
